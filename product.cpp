f411679f43981eb43ee3af082eadbe229bc98fd5
Meenakshi Pal <pal.meenakshi@outlook.com>
2025-05-08 14:29:55 +1000

    product of two numbers

---
+++ product.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int x; int y;
+    cout << "Enter 1st number : ";
+    cin >> x;
+
+    cout << "Enter 2nd number : ";
+    cin >> y;
+
+    cout << "The product is " << x * y;
+}
