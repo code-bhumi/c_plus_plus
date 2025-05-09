17df8cad7b55272614f89ba9cc19565ce8852525
Meenakshi Pal <pal.meenakshi@outlook.com>
2025-05-09 14:55:58 +1000

    how to get the ASCII value of char

---
+++ ascii_value.cpp
@@ -0,0 +1,11 @@
+// how to get ASCII value using typecasting
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    char ch;
+    cin >> ch;
+    cout << (int)ch;
+}
---
+++ typecasting.cpp
@@ -0,0 +1,15 @@
+// half of a number
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int x;
+
+    cout << "Enter a number : ";
+    cin >> x;
+    float y = (float)x;   // this is used for typecasting(if you want to chane from int to float or vice versa)
+    cout << y/2;
+
+}
